e8cc02932e013a227e89f092703ea61bbf370935
non-97 <nonmonaka07@gmail.com>
2022-05-11 09:50:00 +0900

    Initial commit

---
+++ test/vpc-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VpcLambda from '../lib/vpc-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vpc-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VpcLambda.VpcLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
